7681799c5563da877dcc5abca2daf4dfc8068c49
Azhaan Ali <azhaanali@live.com>
2022-05-09 16:33:33 -0700

    Simpler solution thought through.

--- src/Main.java
+++ src/Main.java
@@ -9,42 +9,12 @@ public class Main {
          */
 
         Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
         System.out.println(
-                hasSevenInRow(s.nextLine())?
+                input.contains("0000000") || input.contains("1111111")?
                         "YES" :
                         "NO"
         );
-
-    }
-
-    public static boolean hasSevenInRow(String s){
-
-        char[] asArr = s.toCharArray();
-
-        // If the string is less than 7 characters long, we know there isn't a consecutive line of 7.
-        if(asArr.length < 7) return false;
-
-        // Keep track of the last character.
-        // If it's the same as the last, increment the counter
-        // If it's not the same as the last, then reset the count and reassign the value of last char.
-        int consecutive = 0;
-        char lastChar = asArr[0];
-        for(int i = 1; i < asArr.length; i++)
-        {
-            // Above algorithm.
-            char currentChar = asArr[i];
-            if(lastChar == currentChar)
-                consecutive++;
-            else {
-                consecutive = 0;
-                lastChar = currentChar;
-            }
-            // Check if a string of 7 has been found.
-            if(consecutive == 7) return true;
-        }
-
-        // No string of 7 has been found.
-        return false;
 
     }
 
